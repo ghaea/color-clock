a2e33f63bac6dc6220da3dcb299e57ca1c201e90
Ghaea <ghaea.tam@gmail.com>
2015-06-05 10:28:02 -0400

    save point bg colors changes but time doesn't change to hex

--- index.js
+++ index.js
@@ -8,9 +8,11 @@
 
   It returns a whole number value from 0-255 representing the
   relative CSS RGB value of that time period.
-
-  It's pre-written for you. Best to not muck around with it.
 */
+
+
+//  It's pre-written for you. Best to not muck around with it.
+
 var convertTimeframe = function(amount, unit) {
 
   switch (unit) {
@@ -26,3 +28,49 @@ var convertTimeframe = function(amount, unit) {
       return 0;
   }
 }
+
+// START
+
+var clock = moment().format('hh:mm:ss')
+
+var timeText = $(".time").text(clock)
+
+
+var r = convertTimeframe(moment().format('hh'), "hour")
+var g = convertTimeframe(moment().format('mm'), "minute")
+var b = convertTimeframe(moment().format('ss'), "second")
+
+    $(".clock-container").css("background", "rgb(" + r + "," + g + "," + b +")")
+
+
+
+// DOC START
+
+$(document).on("ready", function() {
+
+// Digital Clock in hh:mm:ss format
+var reloadClock = function() {
+  $('.time').html(moment().format('hh:mm:ss'));
+
+  r = convertTimeframe(moment().format('hh'), "hour")
+  g = convertTimeframe(moment().format('mm'), "minute")
+  b = convertTimeframe(moment().format('ss'), "second")
+    
+  $(".clock-container").css("background", "rgb(" + r + "," + g + "," + b +")")
+  
+}
+
+setInterval(reloadClock, 1000)
+
+/*$(".time").on("click", function() {
+    .format("r + ":" + g ":" b")
+})*/
+
+
+})
+
+
+
+
+
+
